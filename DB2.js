ec7bc3984a625f9bbe3f99b36cb355fc8ccab10d
Christopher Lawrence Golston <122076415+cstashes@users.noreply.github.com>
2025-05-09 09:34:10 -0600

    Update DB2.js

--- DB2.js
+++ DB2.js
@@ -1,7 +1,25 @@
 Game, Release Date, Specs, Genre
 
 
+
+
+
 Maddog Mccree, 1996, CC, Adventure   
 
 
+
+
+
 Dracula Unchained, 1998, cc, Horror
+
+
+
+
+
+Oregon Trail 2, 1997, cc, Simulation
+
+
+
+
+
+
